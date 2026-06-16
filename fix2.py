cc186e76c7579343209f6d8aa9baa39811add6e0
Wender <wenderj333@gmail.com>
2026-06-16 02:42:18 +0200

    fix: emojis corrompidos removidos

---
+++ fix2.py
@@ -0,0 +1,13 @@
+﻿with open(r'C:\Users\wender\Desktop\duelo-biblico\index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+import re
+content = re.sub(r'[\U00010000-\U0010ffff]', '', content)
+content = content.replace('BÃ¡blico', 'Biblico')
+content = content.replace('FÃ©', 'Fe')
+content = content.replace('Ã¡', 'a')
+content = content.replace('Ã©', 'e')
+
+with open(r'C:\Users\wender\Desktop\duelo-biblico\index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+print('OK: ' + str(len(content)) + ' chars')
